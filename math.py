4166767796f1d0b4d33dd43d7930b9fdc3ae5103
student <mjeraeunice@gmail.com>
2023-03-31 19:57:16 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer=a+b
+    return answer
+    
+def subtract(d,e):
+    answer=d-e
+    return answer
+
+def divide(k,l):
+    answer=k/l
+    return answer
+
+def multiply(j,f):
+    answer=j*f
+    return answer
+
+def remainder(x,y):
+    answer=x%y
+    return answer
